6bc0252453dd1df1ed12874237e0cc9d4c6b2029
djamel1133 <djamel1133@gmail.com>
2024-11-15 12:01:34 +0100

    2nd

--- script/script.js
+++ script/script.js
@@ -3,13 +3,13 @@ const authors = [
   name : "Tanya Sinclair" ,
   job: "UX Engineer",
   description:"\“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. \”",
-  image_path: "../images/image-tanya.jpg",
+  image_path: "images/image-tanya.jpg",
  } ,
  {
     name: "John Tarkpor" ,
     job: "Junior Front-end Developer",
     description:"\“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. \”",    
-    image_path: "../images/image-john.jpg",
+    image_path: "images/image-john.jpg",
  }
 ]
 
